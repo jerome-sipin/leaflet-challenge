11da4ce159d3dd1c296f83f1494bfcacecda87a3
jerome-sipin <jerome.sipin15@gmail.com>
2021-10-02 22:04:08 -0400

    updated comments

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,16 +11,13 @@ d3.json(url).then(function (data) {
 
 function createFeatures(earthquakeData) {
 
-  // Function runs for every feature in the array. The popup describes where, when, the magnitude, and the depth of the earthquake.
+  // Function runs for every feature in the array and creates a popup for it. The popup describes where, when, the magnitude, and the depth of the earthquake.
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3> Place: " + feature.properties.place + "</h3><h3>Magnitude:" + feature.properties.mag + "</h3><h3>Depth:" + feature.geometry.coordinates[2] + "</h3><h3>Time:" + new Date(feature.properties.time) + "</h3>")
 
   }
 
-
-
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
+  // Create layer using GeoJSON that displays earthquakes using circles. Changes aspects of the circle depending on information from the features array.
   var earthquakes=  L.geoJson(earthquakeData, {
     pointToLayer: function(feature, layer) {
         
@@ -115,6 +112,9 @@ function createMap(earthquakes) {
 
   var legend = L.control({ position: "bottomright" });
 
+
+  // Creates html div on the map that holds legend information. Styling can be found on in style.css
+  // Code for legend adapted from this source: https://codepen.io/haakseth/pen/KQbjdO
   legend.onAdd = function(map) {
     var div = L.DomUtil.create("div", "legend");
     div.innerHTML += "<h4>Earthquake Depth (km)</h4>";
@@ -132,61 +132,3 @@ function createMap(earthquakes) {
   legend.addTo(myMap);
   
 }
-
-
-
-
-// // Get URL for significant earthquakes over the past month.
-// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-
-// // Perform GET request to query URL
-// d3.json(url).then(function(data){
-//     createMarkers(data.features);
-// })
-
-// // Function to create maps
-// function createMap(eMarkers){
-  
-//     // Create the tile layer that will be the background of our map.
-//     var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//     });
-
-//     var baseMaps = {
-//         "Base Map": streetmap
-//     }
-    
-//     var overlayMaps = {
-//         "Earthquakes": eMarkers
-//     }
-
-//     // Create the map object with options.
-//     var map = L.map("map", {
-//     center: [40.73, -74.0059],
-//     zoom: 12,
-//     layers: [streetmap, eMarkers]
-//      });
-
-//     // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-//     L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//     }).addTo(map);
-// }
-
-// // Adds the markers to map object and creates the map
-// function createMarkers(response){
-    
-//     var earthquakes = response.features;
-
-//     // Create array to hold markers
-//     var eMarkers = [];
-
-//     for (var i = 0; i < earthquakes.length; i++){
-//         var earthquake = earthquakes[i]
-//         var cDate = new Date(earthquake.properties.time).toLocaleDateString("en-US")
-//         var eMarker = L.marker([earthquake.geometry.coordinates[0], earthquake.geometry.coordinates[1]])
-//             .bindPopup("<h3> Place: " + earthquake.properties.place + "</h3><h3>Magnitude:" + earthquake.properties.magType + "</h3><h3>Depth:" + earthquake.geometry.coordinates[2] + "</h3><h3>Time:" + cDate + "</h3>")
-//         eMarkers.push(eMarker);
-//     }
-//     createMap(L.layerGroup(eMarkers));
-// }
